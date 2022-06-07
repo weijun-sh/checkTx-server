2dcf1f136b1e2235caba002733d9828f58de9aa7
weijun-sh <huangweijun0322@163.com>
2022-06-07 16:37:20 +0800

    update api args

--- rpc/rpcapi/rsyslogapi.go
+++ rpc/rpcapi/rsyslogapi.go
@@ -20,25 +20,46 @@ var (
 	bridgeArray_1 = []string{"BTC2BSC", "ETH2BSC", "FSN2BSC", "ETH2FSN", "ETH2Fantom", "FSN2Fantom", "FSN2ETH", "BTC2ETH", "LTC2FSN", "LTC2ETH", "LTC2BSC", "LTC2Fantom", "BLOCK2ETH", "ETH2HT", "FSN2HT", "BTC2HT", "BNB2HT", "Fantom2ETH", "FORETH2Fantom", "HT2BSC", "FORETH2BSC", "FSN2MATIC", "FSN2XDAI", "ETH2XDAI", "ETH2MATIC", "ETH2AVAX", "FSN2AVAX", "BLOCK2AVAX", "BSC2AVAX", "BSC2MATIC", "BSC2ETH", "BSC2Fantom", "Harmony2MATIC", "BTC2Harmony", "COLX2BSC", "Fantom2BSC", "ETH2KCS", "COLX2ETH", "HT2MATIC", "MATIC2BSC", "MATIC2AVAX", "BSC2KCS", "BSC2Harmony", "BSC2OKT", "MATIC2OKT", "BLOCK2MATIC", "BLOCK2BSC", "BSC2MOON", "ETH2MOON", "MATIC2Fantom", "ETH2ARB", "ARB2ETH", "BSC2ARB", "MATIC2MOON", "BSC2IOTEX", "BSC2SHI", "ETH2SHI", "MOON2ETH", "BSC2CELO", "AVAX2Fantom", "ETH2HARM", "HT2Fantom", "ARB2MOON", "AVAX2BSC", "MOON2BSC", "ARB2MATIC", "ETH2TLOS", "CELO2BSC", "TERRA2Fantom", "MOON2SHI", "MATIC2HT", "ETH2IOTEX", "Harmony2BSC", "ETH2MOONBEAM", "BSC2MOONBEAM", "ETH2BOBA", "SHI2BSC", "ETH2astar", "ETH2OKT", "MATIC2ETH", "MATIC2Harmony", "MATIC2MOONBEAM", "AVAX2MOONBEAM", "BSC2astar", "BSC2ROSE", "ETH2ROSE", "ETH2VELAS", "MATIC2XDAI", "IOTEX2BSC", "XRP2AVAX", "ETH2CLV", "ETH2MIKO", "XRP2AVAX", "ETH2MIKO", "ETH2CONFLUX", "KCC2CONFLUX", "ETH2OPTIMISM", "ETH2RSK", "BSC2RSK", "JEWEL2Harmony", "TERRA2ETH", "ETH2EVMOS", "ETH2DOGE", "ETH2ETC", "ETH2CMP"}
 )
 
+type ResultStatus struct {
+	Code uint64 `json:"code"`
+	Msg string `json:"msg"`
+	Data map[string]*GetStatusInfoResult `json:"data"`
+}
+
 type GetStatusInfoResult map[string]interface{}
 
 // GetStatusInfo api
-// TODO
-func (s *RouterSwapAPI) GetStatusInfo(r *http.Request, statuses *string, result *GetStatusInfoResult) error {
-	fmt.Printf("GetStatusInfo, statuses: %v\n", *statuses)
-	//ret := make(map[string]interface{}, 0)
-	for _, dbname := range routerArray_1 {
-		fmt.Printf("\nfind dbname: %v\n", dbname)
-		res, err := swapapi.GetStatusInfo(dbname, *statuses)
-		if err == nil && res != nil {
-			*result = res
-			return nil
+func (s *RouterSwapAPI) GetStatusInfo(r *http.Request, args *GetSwapHistoryArgs, result *ResultStatus) error {
+	result.Code = 0
+	result.Msg = ""
+	result.Data = make(map[string]*GetStatusInfoResult, 0)
+	dbname := args.Bridge
+	status := args.Status
+	if status == "" {
+		status = "0,8,9,12,14,17" // default
+	}
+	fmt.Printf("GetStatusInfo, status: %v\n", status)
+	if dbname == "all" {
+		for _, dbname := range routerArray_1 {
+			getStatusInfo(dbname, status, result)
 		}
+	} else {
+		getStatusInfo(dbname, status, result)
 	}
 	return nil
 }
 
-type ResultBridge struct {
+func getStatusInfo(dbname, status string, result *ResultStatus) {
+	fmt.Printf("\nfind dbname: %v\n", dbname)
+	res, err := swapapi.GetStatusInfo(dbname, status)
+	if err == nil && len(res) != 0 {
+		var s GetStatusInfoResult
+		s = res
+		result.Data[dbname] = &s
+	}
+}
+
+type ResultHistory struct {
 	Code uint64 `json:"code"`
 	Msg string `json:"msg"`
 	Data map[string][]*statusConfig `json:"data"`
@@ -52,19 +73,22 @@ type GetSwapHistoryArgs struct {
 	Status string `json:"status"`
 }
 
-func (s *RouterSwapAPI) GetSwapNotStable(r *http.Request, args *RPCNullArgs, result *ResultBridge) error {
+func (s *RouterSwapAPI) GetSwapNotStable(r *http.Request, args *RPCNullArgs, result *ResultHistory) error {
 	var argsH GetSwapHistoryArgs
 	argsH.Bridge = "all"
 	argsH.Status = "0,8,9,12,14,17" // default
 	return s.GetSwapHistory(r, &argsH, result)
 }
 
-func (s *RouterSwapAPI) GetSwapHistory(r *http.Request, args *GetSwapHistoryArgs, result *ResultBridge) error {
+func (s *RouterSwapAPI) GetSwapHistory(r *http.Request, args *GetSwapHistoryArgs, result *ResultHistory) error {
 	result.Code = 0
 	result.Msg = ""
 	result.Data = make(map[string][]*statusConfig, 0)
 	dbname := args.Bridge
 	status := args.Status
+	if status == "" {
+		status = "0,8,9,12,14,17" // default
+	}
 	fmt.Printf("dbname: %v, status: %v\n", dbname, status)
 	if dbname == "all" {
 		for _, dbname := range routerArray_1 {
@@ -76,7 +100,7 @@ func (s *RouterSwapAPI) GetSwapHistory(r *http.Request, args *GetSwapHistoryArgs
 	return nil
 }
 
-func getSwapHistory(dbname, statuses string, result *ResultBridge) {
+func getSwapHistory(dbname, statuses string, result *ResultHistory) {
 	fmt.Printf("\nfind dbname: %v\n", dbname)
 	parts := strings.Split(statuses, ",")
 	for _, status := range parts {
